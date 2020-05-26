a0acd16f8eba2c8a520e6e07d5128a71b0e7ca61
Jeff Balagosa <jeff.balagosa@gmail.com>
2020-05-25 19:07:20 -0700

    added and tested phrases randomizer

--- js/app.js
+++ js/app.js
@@ -2,8 +2,22 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
 const startButton = document.querySelector('.btn__reset');
+const phrases = [
+  'UP ALL NIGHT',
+  'TWIST OF FATE',
+  'WAY OF LIFE',
+  'ALL THINGS CONSIDERED',
+  'BADGE OF HONOR',
+];
 
 startButton.addEventListener('click', () => {
   const overlay = document.getElementById('overlay');
   overlay.style.display = 'none';
 });
+
+function listPicker(array) {
+  const listItem = Math.floor(Math.random() * Math.floor(array.length));
+  return array[listItem];
+}
+
+console.log(listPicker(phrases));
