d4582a276353df671fca6a74e0cc25162267f1eb
Jeff Balagosa <jeff.balagosa@gmail.com>
2020-05-25 20:46:44 -0700

    now covered word shows up on the board.

--- js/app.js
+++ js/app.js
@@ -22,3 +22,22 @@ function getRandomPhraseAsArray(arr) {
   const phraseAsArray = Array.from(word);
   return phraseAsArray;
 }
+
+// Set the game display.
+const phraseArray = getRandomPhraseAsArray(phrases);
+function addPhraseToDisplay(arr) {
+  // Create an addPhraseToDisplay function that loops through an array of characters. Inside the loop, for each character in the array, you’ll create a list item, put the character inside of the list item, and append that list item to the #phrase ul in your HTML. If the character in the array is a letter and not a space, the function should add the class “letter” to the list item.
+  for (let i = 0; i < arr.length; i++) {
+    const e = arr[i];
+    const li = document.createElement('li');
+    li.textContent = e;
+    if (e !== ' ') {
+      li.className = 'letter';
+    } else {
+      li.className = 'space';
+    }
+    phrase.appendChild(li);
+  }
+}
+
+addPhraseToDisplay(phraseArray);
